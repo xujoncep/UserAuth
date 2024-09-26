7c3cb3ff52f4c99c13ec231a4f35a943f0ffa2a1
sujoncep <sujoncep@gmail.com>
2024-09-27 00:46:32 +0600

    Add project files.

---
+++ UserAuth/ViewModels/LoginViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UserAuth.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage ="Email is required")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name ="Rember me?")]
+        public bool RememberMe { get; set; }
+    }
+}
